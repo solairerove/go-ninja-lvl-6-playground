387f45001195ab964aeb4983a921ada4a5f21247
Mikita Kryvitski <solairerove@gmail.com>
2019-07-12 20:23:46 +0300

    feature: complete ex1 of jedi lvl 6

--- jedi/ex_1.go
+++ jedi/ex_1.go
@@ -7,4 +7,15 @@ import (
 // Ex1 fmt
 func Ex1() {
 	fmt.Println("Ex1")
+
+	fmt.Println(foo())
+	fmt.Println(bar())
+}
+
+func foo() int {
+	return 42
+}
+
+func bar() (int, string) {
+	return 1984, "Big Brother is Watching"
 }
