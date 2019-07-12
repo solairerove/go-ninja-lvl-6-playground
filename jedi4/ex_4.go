e33e59c196af05c8aa31d1c9cd68c924ef0756f8
Mikita Kryvitski <solairerove@gmail.com>
2019-07-12 20:57:49 +0300

    feature: ex4

--- jedi4/ex_4.go
+++ jedi4/ex_4.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 )
 
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func (p person) speak() {
+	fmt.Println("My name is", p.first, p.last)
+}
+
 // Ex4 fmt
 func Ex4() {
 	fmt.Println("Ex4")
+
+	p := person{
+		first: "Tyler",
+		last:  "Durden",
+		age:   23,
+	}
+
+	p.speak()
 }
