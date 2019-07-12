cbdd9a3807d1bef8a7450498c5e1be82e4bd5945
Mikita Kryvitski <solairerove@gmail.com>
2019-07-12 20:45:04 +0300

    feature: complete ex3

--- jedi3/ex_3.go
+++ jedi3/ex_3.go
@@ -7,4 +7,18 @@ import (
 // Ex3 fmt
 func Ex3() {
 	fmt.Println("Ex3")
+
+	defer foo()
+	bar()
+}
+
+func foo() {
+	defer func() {
+		fmt.Println("Who is Tyler Durder?")
+	}()
+	fmt.Println(42)
+}
+
+func bar() {
+	fmt.Println(1984, "Big Brother is Wathincg ya")
 }
