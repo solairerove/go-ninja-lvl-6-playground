75bd191fce8f32ecff03140d3442f555da3c22e4
Mikita Kryvitski <solairerove@gmail.com>
2019-07-12 21:45:50 +0300

    feature: complete ex9 with jack

--- jedi9/ex_9.go
+++ jedi9/ex_9.go
@@ -7,4 +7,23 @@ import (
 // Ex9 fmt
 func Ex9() {
 	fmt.Println("Ex9")
+
+	foo(whoAmI, "Tyler", "Jack")
+}
+
+func whoAmI(name string) {
+	switch name {
+	case "Tyler":
+		fmt.Println("I am Jack")
+	case "Jack":
+		fmt.Println("I am Tyler")
+	default:
+		fmt.Println("Poor Bob")
+	}
+}
+
+func foo(f func(name string), names ...string) {
+	for _, v := range names {
+		f(v)
+	}
 }
