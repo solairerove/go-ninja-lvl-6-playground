91ba6d2910d624d2c8c805b404af514687982848
Mikita Kryvitski <solairerove@gmail.com>
2019-07-12 21:53:10 +0300

    feature: complete ex10

--- jedi10/ex_10.go
+++ jedi10/ex_10.go
@@ -7,4 +7,24 @@ import (
 // Ex10 fmt
 func Ex10() {
 	fmt.Println("Ex10")
+
+	tyler := foo()
+	jack := foo()
+
+	fmt.Println(tyler())
+	fmt.Println(tyler())
+	fmt.Println(jack())
+}
+
+func foo() func() string {
+	s := "Jack"
+
+	return func() string {
+		if s == "Jack" {
+			s = "Tyler"
+		} else {
+			s = "Jack"
+		}
+		return fmt.Sprintln(s)
+	}
 }
