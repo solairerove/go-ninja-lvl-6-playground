c37dae0a31c3a00e86097e25873f066f61daa25d
Mikita Kryvitski <solairerove@gmail.com>
2019-07-12 21:26:29 +0300

    feature: call func ther return func that return tyler

--- jedi8/ex_8.go
+++ jedi8/ex_8.go
@@ -7,4 +7,13 @@ import (
 // Ex8 fmt
 func Ex8() {
 	fmt.Println("Ex8")
+
+	tyler := jack()
+	fmt.Println(tyler())
+}
+
+func jack() func() string {
+	return func() string {
+		return fmt.Sprintln("Who am i?")
+	}
 }
